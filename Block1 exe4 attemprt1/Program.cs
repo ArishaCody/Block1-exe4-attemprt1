2f3a1a7cee839585bca851fe966bd68663fd4b9d
ArishaCody <105153479+ArishaCody@users.noreply.github.com>
2022-05-15 08:47:15 +1000

    Добавьте файлы проекта.

---
+++ Block1 exe4 attemprt1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Block1_exe4_attemprt1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int totalPictures = 52;
+            int picturesInOneRow = 3;
+            int namberOfRows = totalPictures / picturesInOneRow;
+            Console.WriteLine(namberOfRows);
+
+            int usedPictures = namberOfRows * picturesInOneRow;
+            Console.WriteLine(usedPictures);
+
+            int extraPictures = totalPictures - usedPictures;
+            Console.WriteLine(extraPictures);
+        }
+
+    }
+}
